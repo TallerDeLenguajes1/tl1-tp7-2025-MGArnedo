49161a44107456e40d1c1c4799932fc3515d2f31
MGArnedo <mguaarnedo@gmail.com>
2025-05-27 15:12:15 -0300

    Se avanzo con el ejercicio 1: Se creo la clase Calculadora

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,42 @@
+﻿namespace EspacioCalculadora
+{
+    public class Calculadora{
+        private double numero;
+        public Calculadora(double valorInicial=0)
+        {
+            numero=valorInicial;
+        }
+        public void Sumar(double termino)
+        {
+            numero += valorInicial;
+        }
+        public void Restar(double termino)
+        {
+            numero -= valorInicial;
+        }
+        public void Multiplicar(double termino)
+        {
+            numero *= valorInicial;
+        }
+        public void Dividir(double termino)
+        {
+            if (termino!=0)
+            {
+                numero /= valorInicial;
+            }else
+            {
+                throw new DivideByZeroException("No se puede dividir por cero")
+            }
+            public double Resultado()
+            {
+                get => numero;
+            }
+            public void Limpiar()
+            {
+                numero=0;
+            }
+            
+        }
+    }
+}
+
---
+++ Calculadora/Program.cs
@@ -0,0 +1,5 @@
+using EspacioCalculadora;
+class Program
+{
+    Calculadora Nuev
+}
